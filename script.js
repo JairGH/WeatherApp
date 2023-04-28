284f6882e01a0250266b35f5ae17b205af70c45f
Jair Gomez <jairherber505@gmail.com>
2023-04-27 22:48:45 -0700

    is working

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
-// const input = document.querySelector("#search_btn");
-// const apiKey = "1f736240909b453c4de39fd47be912f6";
-// const apiUrl =
-//   "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
-// let searchBox = document.querySelector(".city_search input");
-// let searchBtn = document.querySelector(".city_search button");
-// var today = dayjs().format(" MM/DD/YYYY");
+let input = document.querySelector("#search_btn");
+let apiKey = "1f736240909b453c4de39fd47be912f6";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+let searchBox = document.querySelector(".city_search input");
+let searchBtn = document.querySelector(".city_search button");
+var today = dayjs().format(" MM/DD/YYYY");
 
-// async function weather(city) {
-//   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-//   const data = await response.json();
+async function weather(city) {
+  let response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  let data = await response.json();
 
-//   console.log(data);
+  console.log(data);
+  foreCast(data.coord.lat, data.coord.lon);
+  document.querySelector("#city").innerHTML = data.name + today;
+  document.querySelector("#temp").innerHTML = data.main.temp + "°F";
+  document.querySelector("#wind").innerHTML = data.wind.speed + " MPH";
+  document.querySelector("#humidity").innerHTML = data.main.humidity + " %";
+}
+async function foreCast(lat, lon) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  let response = await fetch(apiUrl);
+  let data = await response.json();
+  console.log(data);
+  display5(data.list);
+}
+function display5(data) {
+  document.getElementById("space").innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+    if (element.dt_txt.includes("06:00:00")) {
+      let panel = document.createElement("div");
+      panel.innerHTML = `<h3>${dayjs(element.dt_txt).format("dddd, DD MM")}</h3>
+        <p>Temp:${element.main.temp}</p>
+        <p >Wind:${element.wind.speed}</p>
+        <p>Humidity:${element.main.humidity}</p>`;
 
-//   document.querySelector("#city").innerHTML = data.name + today;
-//   document.querySelector("#temp").innerHTML = data.main.temp + "°F";
-//   document.querySelector("#wind").innerHTML = data.wind.speed + " MPH";
-//   document.querySelector("#humidity").innerHTML = data.main.humidity + " %";
-// }
-
-// searchBtn = addEventListener("click", () => {
-//   weather(searchBox.value);
-// });
+      document.getElementById("space").append(panel);
+    }
+  }
+}
+searchBtn.addEventListener("click", () => {
+  weather(searchBox.value);
+});
